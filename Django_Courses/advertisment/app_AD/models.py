8943068e45b1da8fb3efc1c103fd31b062acaae5
Brederer <irbistx29@gmail.com>
2023-08-02 15:23:42 +0300

    Course 7

--- Django_Courses/advertisment/app_AD/models.py
+++ Django_Courses/advertisment/app_AD/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class AD(models.Model):
+    title = models.CharField('Название',max_length=128)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('Торг',help_text='Отметьте, если торг уместен')
+    created_at = models.DateTimeField(auto_now_add=True)
+    upated_at = models.DateTimeField(auto_now=True)
+    
